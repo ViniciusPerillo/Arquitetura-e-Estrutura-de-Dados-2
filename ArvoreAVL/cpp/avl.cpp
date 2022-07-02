401d27c9a7e97855df66a6fba5ebb119132a0dd3
Vinicius Perillo <vinicius.perillo25@gmail.com>
2022-07-02 13:34:39 -0300

    feat - functions to insert new elements and funtions to print the AVL

--- ArvoreAVL/cpp/avl.cpp
+++ ArvoreAVL/cpp/avl.cpp
@@ -3,6 +3,8 @@
 ** This code is a AVL Data Structure
 */
 
+//include
+#include <stdexcept>
 
 struct node {
     char content;
@@ -19,14 +21,14 @@ typedef struct node* AVL;
 //prototypes
 
 //main functions
-// AVL newAVL(char, int, AVL);
+AVL newAVL(char, int, AVL);
 // char search(AVL, int);
 // bool insert(AVL, char, int);
 // bool remove(AVL, int);
 // void printAVL(AVL );
 
 //auxiliars
-// bool recursiveInsert(AVL, AVL, AVL);
+bool recursiveInsert(AVL &, AVL &);
 // bool recursiveRemove(AVL, int, AVL);
 // void leftRotation(AVL);
 // void rightRotation(AVL);
@@ -34,7 +36,7 @@ typedef struct node* AVL;
 
 //functions
 
-AVL newAVL(char content, int key, AVL father = nullptr)
+AVL newAVL(char content, int key)
 {
     //creation
     AVL newAVL = new struct node;
@@ -45,7 +47,89 @@ AVL newAVL(char content, int key, AVL father = nullptr)
     newAVL->bal = 0;
     newAVL->left = nullptr;
     newAVL->right = nullptr;
-    newAVL->father = father;
+    newAVL->father = nullptr;
     
     return newAVL;
+}
+
+bool insert(AVL &root, char content, int key)
+{
+    try
+    {
+        //New node
+        AVL newNode = newAVL(content, key);
+        
+        //Insert
+        recursiveInsert(root, newNode);
+    }
+    catch(...)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool recursiveInsert(AVL &root, AVL &newNode)
+{   
+    // case 0: place to insert is found
+    if(root == nullptr)
+    {
+        root = newNode;
+        return true;
+    }
+    
+    // case 1: place to insert is not found
+    if(root->key == newNode->key)
+        throw;
+    
+    
+
+    // case 2: place to insert is on left
+    if(newNode->key < root->key)
+    {
+        if(recursiveInsert(root->left, newNode))
+        {   
+            //Configuration after adding
+            root->left->father = root;
+            root->bal--;
+        }
+    }
+    // case 3: place to insert is on right
+    else if(newNode->key > root->key)
+    {
+        if(recursiveInsert(root->right, newNode))
+        {   
+            //Configuration after adding
+            root->right->father = root;
+            root->bal++;
+        }
+    }
+
+    // balance cases
+
+    // case 0: recursiveInsert caused a height increase
+    if(abs(root->bal) == 1)
+        return true;
+
+    // else 
+    // case 1: recursiveInsert did't cause height increase
+
+    // case 2: recursiveInsert caused a height increase and unbalance AVL
+    if(abs(root->bal) > 1){}
+        //balance(root);
+
+    return false;
+}
+
+
+int getHeight(AVL root)
+{
+    if(root == nullptr)
+        return -1;
+    
+    int leftH = getHeight(root->left);
+    int rightH = getHeight(root->right);
+
+    return leftH > rightH ? leftH : rightH + 1;
 }
--- ArvoreAVL/cpp/teste.cpp
+++ ArvoreAVL/cpp/teste.cpp
@@ -5,11 +5,70 @@
 
 //includes
 #include "avl.cpp"
+#include <iostream>
+
+void printAVL(AVL);
+void recursivePrintAVL(AVL, int, int);
 
 
 int main(){
     
-    AVL sla = newAVL('A', 0);
+    AVL sla = nullptr;
+    insert(sla,'D', 4);
+    insert(sla,'B', 2);
+    insert(sla,'F', 6);
+    insert(sla,'A', 1);
+    insert(sla,'C', 3);
+    //insert(sla,'E', 5);
+    insert(sla,'G', 7);
+    //insert(sla,'G', 3);
+    //insert(sla,'H', 5);
+    //insert(sla,'I', 7);
+    //insert(sla,'J', 9);
+    //insert(sla,'K', 11);
+    //insert(sla,'L', 13);
+    //insert(sla,'M', 15);
+
+    printAVL(sla);
     
     return 0;
+}
+
+/*
+1_2_4_
+3_| |
+5_6_|
+7_|
+
+4 2 1
+    3  
+  6 5
+    7
+
+*/
+
+void printAVL(AVL root){
+    int height = getHeight(root);
+    recursivePrintAVL(root, height, height);
+    std::cout << '\n';
+}
+
+void recursivePrintAVL(AVL root, int totalHeight, int scopeHeight){
+    
+    if(root != nullptr)
+    {
+        recursivePrintAVL(root->left, totalHeight, scopeHeight-1);
+        
+        std::cout << '\n';
+        for(int i = 0; i < totalHeight - scopeHeight; i++)
+            std::cout << "  ";
+        std::cout << " " << root->content;
+
+        recursivePrintAVL(root->right, totalHeight, scopeHeight-1);
+    }
+    else
+    {
+      if(scopeHeight > -1)
+        std::cout << "\n";
+    }
 }
